2824f383fcb682f804882494b9b333090aa8f8ad
Yunhyeok Lee <yunhyeoklee@Yunhyeoks-MacBook-Pro.local>
2017-02-09 14:48:33 -0600

    Pro 1932 is Failed. I have no idea

--- 1932/problem_1932.java
+++ 1932/problem_1932.java
@@ -4,11 +4,13 @@ import java.util.*;
 public class problem_1932 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-
     int i, j;
     int n = scanner.nextInt();
-
     int[][] before = new int[n][n];
+
+    if(n > 500)
+      return;
+
 
     for(i = 0; i < n; i++) {
       for(j = 0; j <= i; j++) {
@@ -23,22 +25,25 @@ public class problem_1932 {
 
     for(i = 0; i < n; i++) {
       for(j = 0; j < i; j++) {
-        if(j==0)
+        if(j<=0) {
           before[i][j] += before[i-1][j];
-        else if(j == i)
+        }
+        else if(j == i-1) {
           before[i][j] += before[i-1][j-1];
-        else
-          before[i][j] = Math.max((before[i-1][j]+before[i][j]),(before[i-1][j]+before[i][j+1]));
+        }
+        else {
+          before[i][j] = Math.max((before[i-1][j]+before[i][j]),(before[i][j]+before[i-1][j-1]));
+        }
       }
     }
 
-    int v = 0;
+    int value = 0;
     for(int tmp = 0; tmp < n; tmp++)
-      if(v < before[n-1][tmp])
-        v = before[n-1][tmp];
+      if(value < before[n-1][tmp] && before[n-1][tmp] !=0)
+          value = before[n-1][tmp];
 
 
-        System.out.println(v);
+        System.out.println(value);
         return;
 
   }
